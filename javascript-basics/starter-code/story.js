caa8917fcd14db3323130c77d12768ac77de5e9b
aj <cchickoryrod@gmail.com>
2026-01-29 20:00:18 -0500

    Kyle's story

--- javascript-basics/starter-code/story.js
+++ javascript-basics/starter-code/story.js
@@ -7,32 +7,48 @@
 
 // TODO: Add your variables here
 // Example: const heroName = "brave adventurer";
-
+const hikerName = "Kyle";
+const location = "Mountain of Magic";
+let birds = 0;
 
 // Step 2: Story Opening
 // Use console.log to display your story's beginning
 
 // TODO: Display your story opening here
-
+console.log("Hello! My name is " + hikerName + ".");
+console.log("I am going to be hiking on the " + location + " today.");
+console.log("I wonder how many animals I will see.");
+birds = birds + 3;
+console.log("Oh! There are some!");
+console.log("Wow! There are " + birds + " birds!");
 
 // Step 3: Story Development
 // Add more variables to track story elements
 // Examples: locations, objects, numbers, true/false facts
 
 // TODO: Add more story variables
-
+let babyFoxes = 0;
+const foxesCute = true;
+const weather = "sunny";
+let rabbitFamily = 5;
+let fatSqurrel = 1;
 
 // Step 4: Story Climax
 // Combine your variables to create exciting moments
 
 // TODO: Build your story's exciting part
-
+console.log("I wonder what I am going to see next.");
+console.log("There are some young foxes!");
+babyFoxes = babyFoxes + 3;
+console.log("I see some other animals, too!");
+console.log("I see " + babyFoxes + " foxes, " + rabbitFamily + " rabbits, and " + fatSqurrel + " chubby squirrel!");
 
 // Step 5: Story Ending
 // Use all your variables to create a personalized ending
 
 // TODO: Create your ending here
-
+console.log("It is getting late, I should get going. But I'll never forget this beautiful, " + weather + " day!");
+console.log("The End-FOR NOW, MWAHAHAAAA!");
 
 // Extension Challenges:
 // - Add math: count points or track scores
